325224b71fe67faa975fecd0450333c6273ecf08
nurmaliaaa <maliaputri4750@gmail.com>
2025-06-05 09:45:05 +0700

    Resolve merge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import logo from "../assets/UPN.png";
+import upnvjLogo from "../assets/upnvj-logo.png"; // pastikan path ini sesuai
 
 const userName = "Mahasiswa UPNVJ";
 
