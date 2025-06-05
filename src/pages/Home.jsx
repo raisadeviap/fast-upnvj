f2581642f266d246c7974755915db84167553d57
nurmaliaaa <maliaputri4750@gmail.com>
2025-06-06 01:13:56 +0700

    Perbaikan Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/UPN.png";
 import { FunnelIcon } from '@heroicons/react/24/solid';
+import HeroImage from "../assets/img1.svg";
 
 const Fasilitas = [
-  { title: 'Auditorium Bhineka Tunggal Ika', kapasitas: '300 orang', gedung: 'Plaza Soedirman' },
+   {
+    title: 'Auditorium Bhineka Tunggal Ika',
+    kapasitas: '300 orang',
+    gedung: 'Plaza Soedirman',
+    image: HeroImage, // simpan hanya path atau import-nya
+  },
   { title: 'Auditorium Wahidin Sudiro Husodo', kapasitas: '100 orang', gedung: 'Wahidin Sudiro Husodo' },
   { title: 'Auditorium Dr. Cipto Mangun Kusumo', kapasitas: '200 orang', gedung: 'Dr. Cipto Mangun Kusumo' },
   { title: 'Auditorium MERCe', kapasitas: '100 orang', gedung: 'MERCe Kampus Limo' },
