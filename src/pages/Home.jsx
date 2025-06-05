59e45b66b8657405be6e25efde2118426cad534a
nurmaliaaa <maliaputri4750@gmail.com>
2025-06-05 23:50:50 +0700

    Perbaikan Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import React from 'react';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import AboutImage from "../assets/UPN.png";
