1a25ff26ec849ee139b05f41f425ff7c5d352005
nurmaliaaa <maliaputri4750@gmail.com>
2025-06-05 23:49:59 +0700

    Perbaikan Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import React from 'react';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import AboutImage from "../assets/UPN.png";
 
 const categories = [
   'Elektronik',
