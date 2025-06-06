a23b992c9c930e024583159408ae7b62216c9d3d
nurmaliaaa <maliaputri4750@gmail.com>
2025-06-06 15:16:05 +0700

    revisi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.jsx';
 import Login from './pages/Login.jsx';
@@ -6,7 +6,8 @@ import Register from './pages/Register.jsx';
 import Home from './pages/Home.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Admin from './pages/Admin.jsx';
-import FasilitasPage from "./pages/FasilitasPage";
+import FasilitasList from './pages/FasilitasList.jsx';
+import FasilitasDetail from './pages/FasilitasDetail.jsx';
 
 function App() {
   return (
@@ -17,13 +18,12 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path='/admin' element={<Admin />} />
-        <Route path="/fasilitas" element={<FasilitasPage />} />
-        <Route path="/fasilitas/:slug" element={<FasilitasPage />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/fasilitas" element={<FasilitasList />} />
+        <Route path="/fasilitas/:slug" element={<FasilitasDetail />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-
-export default App
+export default App;
