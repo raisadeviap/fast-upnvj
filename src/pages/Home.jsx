c8a5de7f0bd907d490f23e7708892cab6b60742d
nurmaliaaa <maliaputri4750@gmail.com>
2025-06-06 15:59:27 +0700

    revisi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/UPN.png";
 import { FunnelIcon } from "@heroicons/react/24/solid";
+import { Fasilitas } from "./dataFasilitas"; // sudah benar
+
 
 const Fasilitas = [
   {
