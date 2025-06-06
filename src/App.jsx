9a6da8a8fcfa5b021b71b33a51871668cf202c8a
nurmaliaaa <maliaputri4750@gmail.com>
2025-06-06 14:43:00 +0700

    revisi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Home from "./pages/Home.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import Admin from "./pages/Admin.jsx";
-import FasilitasDetail from "./pages/FasilitasDetail.jsx";
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Home from './pages/Home.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import Admin from './pages/Admin.jsx';
+import FasilitasDetail from "./pages/FasilitasDetail";
 
 function App() {
   return (
@@ -17,12 +17,13 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/fasilitas/:id" element={<FasilitasDetail />} />
+        <Route path='/admin' element={<Admin />} />
+        <Route path="/fasilitas" element={<FasilitasDetail />} />
         <Route path="/fasilitas/:slug" element={<FasilitasDetail />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
+
+export default App
