e13e5c2e5cf28819b98e9143bbe35ba0329aa838
nurmaliaaa <maliaputri4750@gmail.com>
2025-06-06 13:44:44 +0700

    revisi

--- src/pages/dataFasilitas.js
+++ src/pages/dataFasilitas.js
@@ -1,10 +1,19 @@
 export const Fasilitas = [
   {
-    slug: "lab-komputer",
-    title: "Laboratorium Komputer",
-    gedung: "Gedung A",
-    kapasitas: "30 orang",
-    image: "/images/lab-komputer.jpg",
+    slug: "auditorium-bhineka-tunggal-ika",
+    title: "Auditorium Bhineka Tunggal Ika",
+    kapasitas: "300 orang",
+    gedung: "Plaza Soedirman",
+    deskripsi: "Tempat seminar dan kuliah besar",
+    image: "https://www.upnvj.ac.id/en/files/large/5fe91f59d3da4d824097b0b5bb994e69",
   },
-  // ...data lainnya
+  {
+    slug: "auditorium-wahidin-sudiro-husodo",
+    title: "Auditorium Wahidin Sudiro Husodo",
+    kapasitas: "100 orang",
+    gedung: "Wahidin Sudiro Husodo",
+    deskripsi: "Tempat seminar dan kuliah besar",
+    image: "https://fk.upnvj.ac.id/wp-content/uploads/2022/04/IMG_2038-scaled.jpg",
+  },
+  
 ];
